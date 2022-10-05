e70e07230c796e7ca9b147cd90d29e6818433188
nicoletheillustrator <nicole-park@naver.com>
2022-10-06 02:25:34 +0900

    .

--- src/components/loader.js
+++ src/components/loader.js
@@ -8,7 +8,7 @@ export default function Loader() {
     // until a better solution comes aka more education. what works, works.
 
 
-    window.onload = () => {
+    window.load = () => {
 
          var loaderwolf = anime.timeline({
             easing: 'easeInOutQuart',
@@ -67,51 +67,6 @@ export default function Loader() {
     };
 function test() {
     console.log("test first load")
-
-    
-    var loaderwolf = anime.timeline({
-        easing: 'easeInOutQuart',
-        duration: 1500,
-        opacity: [
-            {value: [0, 1], duration: 200}
-        ],
-
-      
-     });
-     
-    loaderwolf 
-       .add({
-            targets: '.loader-wolf .path-a',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            opacity: 1
-        })
-
-        .add({
-    
-            duration: 1000,
-            targets: '.path-c',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            opacity: 1
-        })
-
-        .add({
-         
-            duration: 500,
-            targets: '.path-b',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            opacity: 1
-        })
-        
-        .add ({
-            delay: 300,
-            targets: '.loader-wolf',
-            keyframes : [
-                {scale: 1.1},
-                {scale: .5}
-            ],
-            easing: 'easeInOutQuint'
-        });
-    
 }
    test()
     return (
