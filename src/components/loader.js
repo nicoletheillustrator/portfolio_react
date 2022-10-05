bc7366ee2078db66bab52f243eea3f2afc33b2b2
nicoletheillustrator <nicole-park@naver.com>
2022-10-06 02:21:11 +0900

    testing load for mobile.

--- src/components/loader.js
+++ src/components/loader.js
@@ -67,6 +67,51 @@ export default function Loader() {
     };
 function test() {
     console.log("test first load")
+
+    
+    var loaderwolf = anime.timeline({
+        easing: 'easeInOutQuart',
+        duration: 1500,
+        opacity: [
+            {value: [0, 1], duration: 200}
+        ],
+
+      
+     });
+     
+    loaderwolf 
+       .add({
+            targets: '.loader-wolf .path-a',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            opacity: 1
+        })
+
+        .add({
+    
+            duration: 1000,
+            targets: '.path-c',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            opacity: 1
+        })
+
+        .add({
+         
+            duration: 500,
+            targets: '.path-b',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            opacity: 1
+        })
+        
+        .add ({
+            delay: 300,
+            targets: '.loader-wolf',
+            keyframes : [
+                {scale: 1.1},
+                {scale: .5}
+            ],
+            easing: 'easeInOutQuint'
+        });
+    
 }
    test()
     return (
