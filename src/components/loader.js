b649192729300d27f17267f5620f1aa4772b0927
nicoletheillustrator <nicole-park@naver.com>
2022-10-06 11:49:09 +0900

    Didn't work.

--- src/components/loader.js
+++ src/components/loader.js
@@ -8,64 +8,68 @@ export default function Loader() {
     // until a better solution comes aka more education. what works, works.
 
 
-//     window.onload = () => {
-// //before
+    window.onload = () => {
 
-//     };
+         var loaderwolf = anime.timeline({
+            easing: 'easeInOutQuart',
+            duration: 1500,
+            opacity: [
+                {value: [0, 1], duration: 200}
+            ],
+
+         });
+         
+        loaderwolf 
+           .add({
+                targets: '.loader-wolf .path-a',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                opacity: 1
+            })
+    
+            .add({
+        
+                duration: 1000,
+                targets: '.path-c',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                opacity: 1
+            })
+    
+            .add({
+             
+                duration: 500,
+                targets: '.path-b',
+                strokeDashoffset: [anime.setDashoffset, 0],
+                opacity: 1
+            })
+            
+            .add ({
+                delay: 300,
+                targets: '.loader-wolf',
+                keyframes : [
+                    {scale: 1.1},
+                    {scale: .5}
+                ],
+                easing: 'easeInOutQuint'
+            });
+        
+
+
+
+        setTimeout(
+            function(){
+            pageRef.current.style.display = 'none';
+           
+            console.log("new loD")
+            
+        }, "4500")
+
+
+    };
 
 
 function test() {
     if (window.onload) {
             console.log("works mobile")
-
-            var loaderwolf = anime.timeline({
-                easing: 'easeInOutQuart',
-                duration: 1500,
-                opacity: [
-                    {value: [0, 1], duration: 200}
-                ],
-    
-             });
-             
-            loaderwolf 
-               .add({
-                    targets: '.loader-wolf .path-a',
-                    strokeDashoffset: [anime.setDashoffset, 0],
-                    opacity: 1
-                })
-        
-                .add({
-            
-                    duration: 1000,
-                    targets: '.path-c',
-                    strokeDashoffset: [anime.setDashoffset, 0],
-                    opacity: 1
-                })
-        
-                .add({
-                 
-                    duration: 500,
-                    targets: '.path-b',
-                    strokeDashoffset: [anime.setDashoffset, 0],
-                    opacity: 1
-                })
-                
-                .add ({
-                    delay: 300,
-                    targets: '.loader-wolf',
-                    keyframes : [
-                        {scale: 1.1},
-                        {scale: .5}
-                    ],
-                    easing: 'easeInOutQuint'
-                });
-            
-    
-    
-    
-
-
-
 
         setTimeout(
             function(){
