3057acc7df081a5284200e0f27bb47dfb60241a5
nicoletheillustrator <nicole-park@naver.com>
2022-10-07 22:46:15 +0900

    time

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
 
 
-    const pageRef = React.useRef()
+    const pageRef = React.createRef()
     const wolfref = React.useRef()
     const ref1 = React.useRef()
     const ref2 = React.useRef()
@@ -15,19 +15,12 @@ export default function Loader() {
       
         setTimeout(
             function(){
-                if(pageRef.current){
-           document.getElementById('loader-wrap').style.display = "none"
-                 }
-            
-            else {
-                console.log("fail")
-            }
-            
+           pageRef.current.style.display = "none";
         }, "3400")
       });
     
     return (
-        <div id="loader-wrap" ref={pageRef} display="flex">
+        <div id="loader-wrap" ref={pageRef} >
         <svg className="loader-wolf" ref={wolfref} viewBox="0 0 91 105" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clipPath="url(#clip0_233_8)">
             <path className="path-a" ref={ref1} d="M45.2601 24.2701L35.3801 23.9301L10.1401 1.08008L7.75009 46.4401L0.590088 69.3001L29.5801 89.4201L32.3101 100.2L45.2601 103.75L58.2001 100.2L60.9301 89.4201L89.9201 69.3001L82.7601 46.4401L80.3701 1.08008L55.1301 23.9301L45.2601 24.2701Z"/>
